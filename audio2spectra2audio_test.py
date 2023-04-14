5aa78b5293c4d44a33953a07d946d12982ce7f6c
tallivm <animbuss@gmail.com>
2023-04-14 22:02:55 +0300

    added multiprocessing

--- audio2spectra2audio_test.py
+++ audio2spectra2audio_test.py
@@ -1,29 +1,67 @@
-import pydub.playback
+import logging
+from multiprocessing import Process, Queue
+import time
+
+import numpy as np
+from pydub import AudioSegment
+import simpleaudio
+
 import utils as u
 from custom_riffusion import SpectrogramConverter, SpectrogramParams
-import logging
-from tqdm import tqdm
+
+
+CONVERTER = SpectrogramConverter(params=SpectrogramParams())
+
+
+def fake_processing(spectrogram: np.ndarray, converter: SpectrogramConverter = CONVERTER) -> AudioSegment:
+    audio = converter.audio_from_spectrogram(spectrogram.T)
+    print('processed a spectrogram')
+    return audio
+
+
+def play_audio(audio: AudioSegment):
+    print('playing a sound')
+    simpleaudio.play_buffer(audio.raw_data,
+                            num_channels=audio.channels,
+                            bytes_per_sample=audio.sample_width,
+                            sample_rate=audio.frame_rate
+                            )
+    time.sleep(5.09)
+
+
+def transformer(eeg_buffer: Queue, audio_buffer: Queue):
+    while True:
+        if eeg_buffer:
+            eeg_data = eeg_buffer.get()
+            audio = fake_processing(eeg_data)
+            audio_buffer.put(audio)
+        time.sleep(0.01)
+
+
+def player(audio_buffer: Queue):
+    while True:
+        if audio_buffer:
+            audio = audio_buffer.get()
+            play_audio(audio)
 
 
 if __name__ == '__main__':
-
-    logging.info('Initializing Spectrogram Converter...')
-    converter = SpectrogramConverter(params=SpectrogramParams())
+    logging.basicConfig(level=logging.DEBUG)
 
     logging.info('Loading sample WAV file...')
-    test_audio = pydub.AudioSegment.from_wav("../../other/Oriental_Dance.wav")
+    test_audio = AudioSegment.from_wav("../../other/Oriental_Dance.wav")
 
-    logging.info('Getting its spectrogram...')
-    test_spectrograms = converter.spectrogram_from_audio(test_audio)
+    logging.info('Getting its spectrograms...')
+    test_spectrogram = CONVERTER.spectrogram_from_audio(test_audio)[0]
+    spectrogram_parts = u.cut_array(test_spectrogram.T, test_spectrogram.shape[1] - 512, 512)
 
-    for i, spectrogram in enumerate(test_spectrograms):
-        logging.info(f'Working with spectrogram {i}...')
-        spectrogram_parts = u.cut_array(spectrogram.T, spectrogram.shape[1] - 512, 512)
-        audios = []
-        for part in tqdm(spectrogram_parts, desc='Processing parts'):
-            test_audio = converter.audio_from_spectrogram(part.T)
-            audios.append(test_audio)
+    eeg_buffer, audio_buffer = Queue(), Queue()
+    transformer_process = Process(target=transformer, args=(eeg_buffer, audio_buffer))
+    player_process = Process(target=player, args=(audio_buffer,))
 
-        logging.info(f'Playing spectrogram {i}...')
-        for audio in audios:
-            pydub.playback.play(audio)
+    player_process.start()
+    transformer_process.start()
+
+    for spectrogram in spectrogram_parts:
+        eeg_buffer.put(spectrogram)
+        time.sleep(0.1)
