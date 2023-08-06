b404aee712457b73d2dc2f15165ba0b8e7251c0d
Tallivm <animbuss@gmail.com>
2023-08-06 13:51:16 +0300

    init dash examples

--- src/tests/dash_app.py
+++ src/tests/dash_app.py
@@ -9,6 +9,12 @@ from dash.dependencies import Input, Output
 def start_dash(parameter_queue: Queue) -> None:
     app = dash.Dash(__name__)
 
+    NUM_CHANNELS = 8
+
+    channel_checklist_options = [
+        {'label': f'Channel {i}', 'value': i} for i in range(1, NUM_CHANNELS+1)
+    ]
+
     app.layout = html.Div([
         html.H1('Hello, Dash!'),
         html.Div('This is a simple example of a Dash app.'),
@@ -16,11 +22,8 @@ def start_dash(parameter_queue: Queue) -> None:
         html.H1('Checkbox Example'),
         dcc.Checklist(
             id='my-checkbox',
-            options=[
-                {'label': 'Option 1', 'value': 'option1'},
-                {'label': 'Option 2', 'value': 'option2'}
-            ],
-            value=[]
+            options=channel_checklist_options,
+            value=list(range(1, NUM_CHANNELS+1))
         ),
         html.Div(id='output')
     ])
@@ -29,14 +32,8 @@ def start_dash(parameter_queue: Queue) -> None:
         Output('output', 'children'),
         Input('my-checkbox', 'value'),
     )
-    def update_output(value):
-        if 'option1' in value:
-            print('You have selected Option 1')
-        elif 'option2' in value:
-            print('You have selected Option 2')
-        else:
-            print('Please select an option')
-
-        print("Value: ", ", ".join(value))
+    def update_output(value: list[int]):
+        print("Value: ", ", ".join(str(x) for x in value))
+        parameter_queue.put(value)
 
     app.run(debug=True, port=4560)  # Debug=True seems to start main a second time
